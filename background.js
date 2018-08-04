61404b98f140c30680e5803850d2c9ffbd54a950
amastin-microcenter <41840975+amastin-microcenter@users.noreply.github.com>
2018-08-03 20:21:21 -0500

    Update background.js

--- background.js
+++ background.js
@@ -3,6 +3,7 @@ chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
           pageUrl: {hostEquals: 'mei.ultiprotime.com'},
+          //css: ["span[id='???']"]
         })
         ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
