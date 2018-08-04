91677a36154fef2f186f9592bb76aba0accee6ee
amastin-microcenter <41840975+amastin-microcenter@users.noreply.github.com>
2018-08-04 14:28:55 -0500

    Update background.js

--- background.js
+++ background.js
@@ -2,7 +2,7 @@
 chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'amastin-microcenter.github.io'}, //mei.ultiprotime.com
+          pageUrl: {hostEquals: 'mei.ultiprotime.com'}, //mei.ultiprotime.com
           //css: ["span[id='???']"]
         })
         ],
@@ -10,15 +10,5 @@ chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       }]);
     });
 
-function showDOM(domContent){
-      console.log('I received the following DOM content:\n" + domContent);
-}
-
-chrome.browserAction.onClicked.addListener(function (tab) {
-  // ...if it matches, send a message specifying a callback too
-  console.log("This code has been reached");
-  chrome.tabs.sendMessage(tab.id, {text: 'report_back'}, showDOM);
-    
-});
 
 
