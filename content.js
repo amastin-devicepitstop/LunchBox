52cae2172f3017ae45e3208fbe58da3d9be7d5a3
amastin-microcenter <41840975+amastin-microcenter@users.noreply.github.com>
2018-08-03 20:51:03 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,3 +1,5 @@
+console.log("Content.js");
+
 // Listen for messages
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     // If the received message has the expected format...
