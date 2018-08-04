a5482743516990cf81d6ca767ccf825c43a322b5
cd3770rn <cd3770rn@go.minneapolis.edu>
2018-08-03 23:56:20 -0500

    Updated content.js

--- content.js
+++ content.js
@@ -1,12 +1,57 @@
-console.log("Waiting for message...");
-console.log(document.document.getElementsByTagName("button")[0]);
+function getTime(){
+    let today = new Date();
+    let currentTime = new Date();
 
-// Listen for messages
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-    // If the received message has the expected format...
-    if (msg.text === 'report_back') {
-        // Call the specified callback, passing
-        // the web-page's DOM content as argument
-        sendResponse(document.all[0].outerHTML);
+    let currentHour = today.getHours();
+    let currentMinute = today.getMinutes();
+
+    let meridiem;
+    currentHour < 12 ? meridiem = "AM" : meridiem = "PM"; // set meridiem to AM if currentHour is before noon, else PM
+
+    // Converts 24-hour time to 12-hour time and sets current hour to that.
+    currentTime.setHours(currentHour % 12);
+
+    // Formats currentMinute so that its properly readable.
+    // ex. currentMinute = 7 ---> 07
+    if (currentMinute < 10){
+        currentMinute = "0" + currentMinute;
     }
-});
+
+    currentTime.setMinutes(currentMinute);
+
+    return currentTime.getHours() + ":" + currentTime.getMinutes() + ":" + today.getSeconds() + " " + meridiem;
+}
+
+// Wait for window to load
+window.onload = function() {
+    console.log("===== LUNCHBOX =====");
+    console.log("Extension Loaded.");
+
+
+    /*console.log("===== LUNCHBOX: USER =====");
+    var user = document.getElementsById("userName");
+    console.log(user);
+    console.log("====================");*/
+
+    /* getElementsByTagName returns an array of the specified elements.
+     * In order to choose a specific element, you have to specify it's index.
+     * The code below sets the 'Clock On' and 'Clock Off' buttons to variables
+     */
+
+    let clockOn = document.getElementsByTagName("button")[0];
+    let clockOff = document.getElementsByTagName("button")[1];
+
+    // Add an EventListener to listen for clicks on each of the buttons
+    clockOn.addEventListener('click', function() {
+        console.log("Clocked on at " + getTime());
+    }, false);
+
+    clockOff.addEventListener('click', function() {
+        console.log("Clocked off at " + getTime());
+    }, false);
+};
+
+/* Figure out some way to access the GitHub API
+ * Ultimately you will need to incorporate the API tokens from GitHub Apps (https://github.com/settings/applications/833784)
+ * Updating a file on GitHub uses PUT (https://developer.github.com/v3/repos/contents/#update-a-file)
+ */
