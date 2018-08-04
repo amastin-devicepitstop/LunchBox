b9d2363eb76f3cff418536ffddd349632fc46492
amastin-microcenter <41840975+amastin-microcenter@users.noreply.github.com>
2018-08-04 14:21:58 -0500

    Update background.js

--- background.js
+++ background.js
@@ -2,7 +2,7 @@
 chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'cd3770rn.github.io'}, //mei.ultiprotime.com
+          pageUrl: {hostEquals: 'amastin-microcenter.github.io'}, //mei.ultiprotime.com
           //css: ["span[id='???']"]
         })
         ],
