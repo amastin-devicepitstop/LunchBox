3d3b6d837ae932e96cb878146f64ee5fef126d8f
amastin-microcenter <41840975+amastin-microcenter@users.noreply.github.com>
2018-08-17 13:24:33 -0500

    Update default.js

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,8 +1,15 @@
-//function formatHeader(){
+function formatHeader(){
   let today = new Date();
   let month = today.getMonth();
   let day = today.getDay();
   let year = today.getYear();
+  console.log(document.getElementsByTagName("h1"));
   document.getElementsByTagName("h1")[0].innerHTML = month + " " + day + ", " + year;
-//}
+}
+
+window.onload = function(){
+  formatHeader();
+}
+
+
 
