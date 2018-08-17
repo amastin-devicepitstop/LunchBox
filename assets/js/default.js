2e1dd025d5ba68ae535c33b2f5c0b133c6a39206
amastin-microcenter <41840975+amastin-microcenter@users.noreply.github.com>
2018-08-17 14:06:59 -0500

    Update default.js

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,3 +1,9 @@
+const gh = new GitHub({
+    token: '67cfcd62766fd764a23d'
+});
+
+const repo = gh.getRepo("amastin-microcenter", "LunchBox");
+
 function formatHeader(){
     let locale = "en-us";
     var options = { month: 'long', day: 'numeric', year: 'numeric' };
