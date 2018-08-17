ea11401f10c6e6d88180c47e5ae3c7084e0124ff
amastin-microcenter <41840975+amastin-microcenter@users.noreply.github.com>
2018-08-17 13:16:34 -0500

    Update default.js

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,3 +1,6 @@
 let header = document.getElementsByClassName("header-level-1");
-let headerContents = header[0];
-console.log(headerContents);
+console.log(header);
+console.log(header[0]);
+console.log(header[0].innerText);
+console.log(header[0].innerHTML);
+console.log(header[0].textContent);
