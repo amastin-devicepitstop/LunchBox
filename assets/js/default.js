d6cfb21f039642654d3f06c3053d74a0a56f1aac
amastin-microcenter <41840975+amastin-microcenter@users.noreply.github.com>
2018-08-17 13:47:46 -0500

    Update default.js

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,44 +1,5 @@
-function formatMonth(date){
-    console.log(date);
-    switch (date) {
-        case 0:
-            return "January";
-        case 1:
-            return "February";
-        case 2:
-            return "March";
-        case 3:
-            return "April";
-        case 4:
-            return "May";
-        case 5:
-            return "June";
-        case 6:
-            return "July";
-        case 7:
-            return "August";
-        case 8:
-            return "September";
-        case 9:
-            return "October";
-        case 10:
-            return "November";
-        case 11:
-            return "December";
-        default:
-            return "Invalid Date";
-    }
-            
-}
-
 function formatHeader(){
-    let today = new Date();
-    let month = today.getMonth();
-    let day = today.getDay();
-    let year = today.getYear();
-    console.log(today.getYear());
-    console.log(today.getDate());
-    document.getElementsByTagName("h1")[1].innerHTML = formatMonth(month) + " " + day + ", " + year;
+    document.getElementsByTagName("h1")[1].innerHTML = new Date().toLocaleString().split(',')[0];
 }
 
 window.onload = function(){
