873b4344bf28d59daa0e3f75a3ae981ec13ce010
amastin-microcenter <41840975+amastin-microcenter@users.noreply.github.com>
2018-08-17 13:54:04 -0500

    Update default.js

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,6 +1,6 @@
 function formatHeader(){
     let locale = "en-us";
-    document.getElementsByTagName("h1")[1].innerHTML = new Date().toLocaleString(locale, {month: "long"}).split(',')[0];
+    document.getElementsByTagName("h1")[1].innerHTML = new Date().toLocaleString(locale, {month: "long"});
 }
 
 window.onload = function(){
